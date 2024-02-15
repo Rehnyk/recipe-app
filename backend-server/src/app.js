6cc4272e887b33547ee0e628ba0c96d113457525
Rehnyk <hnykova.renata@gmail.com>
2024-02-15 13:13:16 +0200

    Final changes for docker files

--- backend-server/src/app.js
+++ backend-server/src/app.js
@@ -6,10 +6,10 @@ app.get('/', (req, res) => {
               <H2 style="color:white">This is the Backend Server!</H2>
               <p style="color:white">Runs on Node.js and Express.js</p>
               <p style="color:white">Port: 3001 </p>
-              <code style="color:yellow">Remember everytime that you change a file in the backend server, 
-              you need to restart the server to see the changes.</code>
+             
               </body>`);
 });
+
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
