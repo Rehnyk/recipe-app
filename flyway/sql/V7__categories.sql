83bf37ad52539a6c2faadfeed24c3e950b943fdd
Shawn2516 <shawnmusick@gmail.com>
2024-02-15 12:32:26 +0200

    Final changes for docker files with database

--- flyway/sql/V7__categories.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE categories (
-                            category_id SERIAL PRIMARY KEY,
-                            name VARCHAR(255) NOT NULL,
-                            created_at TIMESTAMP NOT NULL,
-                            updated_at TIMESTAMP NOT NULL
-);
--- flyway/sql/V8__recipes_categories.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE recipes_categories (
-                                    recipe_id INT NOT NULL,
-                                    category_id INT NOT NULL,
-                                    created_at TIMESTAMP NOT NULL,
-                                    updated_at TIMESTAMP NOT NULL,
-                                    PRIMARY KEY(recipe_id, category_id),
-                                    FOREIGN KEY (recipe_id) REFERENCES recipes(recipe_id),
-                                    FOREIGN KEY (category_id) REFERENCES categories(category_id)
-);
