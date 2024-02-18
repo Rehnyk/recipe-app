3ee223afa4951cc1137e8f3f45b95a5863111772
Rehnyk <hnykova.renata@gmail.com>
2024-02-18 15:04:37 +0200

    cleanup => backend-server file structure, db connection, Docker files configuratrion

--- frontend-server/vite.config.js
+++ frontend-server/vite.config.js
@@ -2,13 +2,9 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-<<<<<<< Updated upstream:ui/vite.config.js
-	plugins: [sveltekit()]
-=======
 	plugins: [sveltekit()],
 	server: {
 		host: true,
 		port: 9000
 	}
->>>>>>> Stashed changes:frontend-server/vite.config.js
 });
