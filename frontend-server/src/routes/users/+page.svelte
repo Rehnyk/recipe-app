16aa9027241a2e71fbb03433e10f353b17b6a2a4
Rehnyk <hnykova.renata@gmail.com>
2024-02-20 19:20:19 +0200

    Adding example for loading data [in-progress]

--- frontend-server/src/routes/users/+page.svelte
+++ frontend-server/src/routes/users/+page.svelte
@@ -1,14 +1,19 @@
 <script>
-  import '../../styles/global.css';
   import Navigation from '../../components/navigation.svelte';
   import Breadcrumb from '../../components/breadcrumb.svelte';
   import Recipelist from '../../components/recipelist.svelte';
-
-  let data;
+   /** @type {import('./$types').PageData} */
+  export let data;
 </script>
 
-<h1>Hello world from front end users page</h1>
-
+<h1>We try to load some data from server:</h1>
+<ul>
+    {#each data.results as r}
+        <li>
+            [{r.id}]: {r.name}
+        </li>
+    {/each}
+</ul>
 <style>
 
 </style>
