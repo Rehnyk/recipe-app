b3a93f2e0b6c156e294ecb4bb4137c5e0e74b191
Rehnyk <hnykova.renata@gmail.com>
2024-02-28 19:21:17 +0200

    Ratings: Frontend rating-service.

--- frontend-server/src/services/rating-service.js
+++ frontend-server/src/services/rating-service.js
@@ -1,9 +1,49 @@
-//const baseUrl = '/api/recipe/rating';
 const baseUrl = 'http://localhost:3001/api/recipe/rating';
 
-export const findRecipeRating = async (id) => {
-    const response = await fetch(`${baseUrl}/${id}`);
+export const findRecipeRating = async (recipeId) => {
+    const response = await fetch(`${baseUrl}/${recipeId}`);
 
     console.log('RESPONSE:', response);
     return response.json();
+};
+
+export const findUserRecipeRating = async (recipeId, userId) => {
+    const response = await fetch(`${baseUrl}/${recipeId}/${userId}`);
+
+    console.log('RESPONSE:', response);
+    return response.json();
+};
+
+export const addRating = async (recipeId, userId, stars) => {
+    const response = await fetch(`${baseUrl}/${recipeId}`,
+        { method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({'userId': userId, 'stars': stars})
+        });
+
+    console.log('RESPONSE:', response);
+    return response.json();
+};
+
+export const changeRating = async (recipeId, userId) => {
+    const response = await fetch(`${baseUrl}/${recipeId}/${userId}`,
+        { method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({'stars': stars})
+        });
+
+    console.log('RESPONSE:', response);
+    return response.json();
+};
+
+export const removeRating = async (recipeId, userId) => {
+    const response = await fetch(`${baseUrl}/${recipeId}/${userId}`,
+        { method: 'DELETE'});
+
+    console.log('RESPONSE:', response);
+    return response;
 };
